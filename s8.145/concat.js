96b0b1b880ecc889955892e5e7da17de7ca3879c
Andre Guilherme Gomes do Nascimento <andre.gnascimento@csu.com.br>
2024-03-07 13:00:20 -0300

    Finalizando array

---
+++ s8.145/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Georgia', 'Marina']
+const filhos = ['Cleiton', 'Pietro']
+
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6,7]]))
